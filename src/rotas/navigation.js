18eee6e41e74738ac007960a94200c3c9a41e90d
Luiz Almada <luizalmada@yahoo.com>
2024-03-29 02:32:34 -0300

    Parte II

--- src/rotas/navigation.js
+++ src/rotas/navigation.js
@@ -1,9 +1,36 @@
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Home from "./../paginas/Home/index";
+import Login from "./../paginas/Login/index";
+import Cadastro from './../paginas/Cadastro/index';
 
-export default function Navigation(){
+const Stack = createStackNavigator();
+
+export default function Navigation() {
   return (
     <NavigationContainer>
-      
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          //retira o titulo de todas as rota da Tela
+          headerTitle: "",
+          //retira a barra do titulo de todas as rota da Tela
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          // options={{
+          //   //retira o titulo da rota da Tela
+          //   headerTitle: '',
+          //   //retira a barra do titulo da rota da Tela
+          //   headerShown: false
+          // }}
+        />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Cadastro" component={Cadastro} />
+      </Stack.Navigator>
     </NavigationContainer>
-  )
+  );
 }
